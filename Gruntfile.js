1d4be2643a19abca0fee4dcca825817774ec026e
kunigami <kunigami@gmail.com>
2016-07-17 21:58:55 -0700

    cleaning up gruntfile. stop using uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,37 @@
 module.exports = function(grunt) {
 
-  var taskList = ['systemjs', 'uglify'];
+  var DEFAULT_PROJECT = 'earthquake';
+
+  var configsBySource = {
+    earthquake: {
+      src: './01_earthquake/earthquake.js',
+      dest: './dist/earthquake/earthquake.js'
+    },
+  }
+
+  var taskList = ['uglify', 'systemjs'];
+
+  var projectName = grunt.option('project') || DEFAULT_PROJECT;
+  var projectConfig = configsBySource[projectName];
+
+  if (!projectConfig) {
+    console.error("Project doesn't have a configuration: " + projectName);
+  }
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       browser_js: {
         files: ['**/*.js', '!node_modules/**/*', '!dist/**/*'],
-        tasks: taskList,
+        tasks: taskList
       }
     },
     systemjs: {
-        options: {
-            'configFile': './config.js'
-        },
-        dist: {
-            'src': './01_earthquake/earthquake.js',
-            'dest': './dist/earthquake.js'
-        }
+      options: {
+        'configFile': './config.js',
+        minify: true
+      },
+      dist: projectConfig
     },
     jshint: {
         files: [
@@ -29,13 +43,6 @@ module.exports = function(grunt) {
         options: {
             'esnext': true,
         }
-    },
-    uglify: {
-        compact: {
-            files: {
-                './dist/earthquake.min.js': ['./dist/earthquake.js']
-            }
-        }
     }
   });
 
@@ -43,6 +50,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks("grunt-systemjs-builder");
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.registerTask('default', taskList);
   grunt.registerTask('lint', ['jshint']);
 };
